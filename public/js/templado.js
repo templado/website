63a22202116096e5b42f4657632ccbcb18b6a360
Arne Blankerts <Arne@Blankerts.de>
2017-11-25 12:34:47 +0100

    Update some design, turn buttons into links

--- public/js/templado.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function(){
-    var list = document.querySelectorAll('.php');
-    [].forEach.call(list, function(code) {
-        hljs.highlightBlock(code);
-    });
-});
-
